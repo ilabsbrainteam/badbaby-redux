844b505936b9842be8b369991499002d99c828cc
Daniel <dan@mccloy.info>
2024-04-24 14:45:44 -0700

    handle external surrogate ERMs

--- prep-dataset/find-surrogate-erms.py
+++ prep-dataset/find-surrogate-erms.py
@@ -9,7 +9,9 @@ import numpy as np
 from mne.utils import set_log_level
 from mne.io import read_info
 
+# config
 set_log_level("WARNING")
+erm_days_threshold = 2  # if no same-day ERM, how many days before/after to look?
 
 
 def get_meas_date(filepath):
@@ -35,42 +37,61 @@ missing_erms = [f"bad_{x.strip()}" for x in missing_erms]
 
 
 available_erms = defaultdict(list)
-
-# first pass: catalog what dates we have ERMs for
+pattern = r".*_erm(_raw)?.fif$"
+# catalog what dates we have ERMs for
 for _dir in subj_dirs:
-    # see if there's an ERM
+    # see if there's an ERM recorded specifically for this project
     for _fname in (_dir / "raw_fif").iterdir():
-        if re.match(r".*_erm(_raw)?.fif$", _fname.name):
+        if re.match(pattern, _fname.name):
             # don't rely on the datestring in the folder; load the file metadata
             scan_date = get_meas_date(_fname)
             available_erms[scan_date].append(_fname)
-
+# see if there's an ERM from a concurrent project
+for _fname in (root / "extra-data").iterdir():
+    if re.match(pattern, _fname.name):
+        scan_date = get_meas_date(_fname)
+        available_erms[scan_date].append(_fname)
 
 matched_erm_dates = ["date,recipient,donor"]
 missing_erm_dates = list()
 
-# second pass: look for date matches for sessions missing ERMs
+# look for date matches for sessions missing ERMs
 for subj_id in missing_erms:
+    target_date = None
+    # assume all recordings for a "session" are from the same day
     for _fname in (ermsource / subj_id / "raw_fif").glob("*.fif"):
         target_date = get_meas_date(_fname)
-
         break
+    assert target_date is not None, target_date
     if target_date in available_erms:
         matching_erm = available_erms[target_date][0].name
         with add_to_log:
-            print(f"same-day surrogate ERM found for {subj_id}: {matching_erm}")
+            print(f"using {matching_erm} (same-day) as surrogate ERM for {subj_id}")
         matched_erm_dates.append(f"{target_date},{subj_id},{matching_erm}")
     else:
-        diffs = np.abs(np.array(list(available_erms)) - target_date)
-        idx = np.argmin(diffs)
-        offset = diffs[idx]
-        nearest = available_erms[np.array(list(available_erms))[idx]]
-        with add_to_log:
-            print(
-                f"no same-day surrogate ERM for {subj_id}; "
-                f"nearest is {offset.days} days ({'/'.join(nearest[0].parts[-2:])})"
-            )
-        missing_erm_dates.append(target_date)
+        available_erm_dates = np.array(list(available_erms))
+        diffs = available_erm_dates - target_date
+        idx = np.argmin(np.abs(diffs))
+        timedelta = diffs[idx]
+        nearest = available_erms[available_erm_dates[idx]][0].name
+        n_days = np.abs(timedelta.days)
+        _s = "s" if n_days > 1 else ""
+        bef_aft = "before" if np.sign(timedelta.days) < 0 else "after"
+        if n_days <= erm_days_threshold:
+            matched_erm_dates.append(f"{target_date},{subj_id},{nearest}")
+            with add_to_log:
+                print(
+                    f"using {nearest} ({n_days} day{_s} {bef_aft}) "
+                    f"as surrogate ERM for {subj_id}"
+                )
+        else:
+            with add_to_log:
+                print(
+                    f"no acceptable surrogate ERM for {subj_id}; threshold is "
+                    f"{erm_days_threshold} day{'s' if erm_days_threshold > 1 else ''} "
+                    f"and nearest is {n_days} day{_s} {bef_aft} ({nearest})"
+                )
+            missing_erm_dates.append(target_date)
 
 with open(outdir / "log-of-surrogate-erms.txt", "w") as fid:
     logfile.seek(0)
--- prep-dataset/link-surrogate-erms.py
+++ prep-dataset/link-surrogate-erms.py
@@ -10,8 +10,11 @@ df = pd.read_csv(Path("qc") / "erm-surrogates.csv", index_col=False)
 
 for _, row in df.iterrows():
     donor_file = row["donor"]
-    donor = "_".join(donor_file.split("_")[:2])
+    if donor_file.startswith("bad_"):
+        donor = "_".join(donor_file.split("_")[:2])
+        source = root / donor / "raw_fif" / donor_file
+    else:
+        source = root / ".." / "extra-data" / donor_file
     recip = row["recipient"]
-    source = root / donor / "raw_fif" / donor_file
     target = root / recip / "raw_fif" / f"{recip}_erm_raw.fif"
     run(["cp", "-ln", "--preserve=all", str(source), str(target)])
