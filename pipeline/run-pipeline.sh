dbdfac1a43273ceec80edcb37bbbb79e5c149929
Daniel <dan@mccloy.info>
2025-03-25 10:23:56 -0700

    change runner script steps to break after epoching

--- pipeline/run-pipeline.sh
+++ pipeline/run-pipeline.sh
@@ -27,10 +27,10 @@ case "$2" in
         STEPS="$2"
         ;;
     pre)
-        STEPS=init,preprocessing/_01_data_quality,preprocessing/_02_head_pos,preprocessing/_03_maxfilter,preprocessing/_04_frequency_filter,preprocessing/_05_regress_artifact,preprocessing/_06a1_fit_ica,preprocessing/_06a2_find_ica_artifacts
+        STEPS=init,preprocessing/_01_data_quality,preprocessing/_02_head_pos,preprocessing/_03_maxfilter,preprocessing/_04_frequency_filter,preprocessing/_05_regress_artifact,preprocessing/_06b_run_ssp,preprocessing/_07_make_epochs
         ;;
     post)
-        STEPS=preprocessing/_06b_run_ssp,preprocessing/_07_make_epochs,preprocessing/_08a_apply_ica,preprocessing/_08b_apply_ssp,preprocessing/_09_ptp_reject,sensor,source
+        STEPS=preprocessing/_08b_apply_ssp,preprocessing/_09_ptp_reject,sensor,source
         ;;
     all)
         STEPS=init,preprocessing,sensor,source
